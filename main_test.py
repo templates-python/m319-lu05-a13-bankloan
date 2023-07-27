2ea48033ef1ed0c2e2891cf4cf2c42c303ad7c27
Marcel Suter <marcel.suter@bzz.ch>
2023-07-27 13:12:11 +0200

    added tests

--- main_test.py
+++ main_test.py
@@ -1,9 +1,24 @@
 from main import main
 
 
-def test_bar(capsys, monkeypatch):
-    inputs = iter([3])
+def test_positive(capsys, monkeypatch):
+    inputs = iter([28, 52100])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
-    assert captured.out == 'The number is positive.\n'
+    assert captured.out == 'Wir sind in der Lage, Ihnen einen Kredit anzubieten.\n'
+
+def test_young(capsys, monkeypatch):
+    inputs = iter([20, 135750])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Leider können wir Ihnen zur Zeit keinen Kredit anbieten.\n'
+
+
+def test_income(capsys, monkeypatch):
+    inputs = iter([68, 46500])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Leider können wir Ihnen zur Zeit keinen Kredit anbieten.\n'

8c67fcf6a6d17bed74d448126fe3d3103d2fb2b7
Marcel Suter <marcel.suter@bzz.ch>
2023-07-27 13:11:26 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    age = int(input('Wie alt sind sie?'))
+    income = int(input('Wie hoch ist ihr Jahresgehalt?'))
+
+    if age >= 21 and income >= 48000:
+        print('Wir sind in der Lage, Ihnen einen Kredit anzubieten.')
+    else:
+        print('Leider können wir Ihnen zur Zeit keinen Kredit anbieten.')
 
 
 if __name__ == '__main__':
